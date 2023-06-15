4f288b581f8e7ef1c34cd635fc98d2322e2f71fd
Petromir <petromir@devlabs.bg>
2023-06-15 12:29:31 +0300

    add: cors to dev server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const app = express();
-const PORT = 5000;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const PORT = 5000
 
-app.use(express.static('build'));
+app.use(cors())
+app.use(express.static('build'))
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
